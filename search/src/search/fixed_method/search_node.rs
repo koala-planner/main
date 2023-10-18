f2dbd7f74eca40a10746c0dae55b301398c9818d
Mohammad Yousefi <u7700356@anu.edu.au>
2023-10-18 15:34:26 +1100

    added test to node expansion

--- search/src/search/fixed_method/search_node.rs
+++ search/src/search/fixed_method/search_node.rs
@@ -35,11 +35,8 @@ impl SearchNode {
         let mut expansions = self.expand_primitives(primitive_tasks);
         // expand all abstract tasks
         for abstract_id in abstract_tasks.iter() {
-            let search_node = self.expand_abstract_task(*abstract_id);
-            expansions.push(NodeExpansion {
-                connection_label: ConnectionLabel::Decomposition,
-                items: search_node, connector: Connector::OR
-            })
+            let abstract_expansions = self.expand_abstract_task(*abstract_id);
+            expansions.extend(abstract_expansions);
         }
         expansions
     }
@@ -88,18 +85,23 @@ impl SearchNode {
     } 
 
     // TODO: test
-    fn expand_abstract_task(&self, task_id: u32) -> Vec<SearchNode> {
+    fn expand_abstract_task(&self, task_id: u32) -> Vec<NodeExpansion> {
         if let Task::Compound(t) = self.tn.get_task(task_id).unwrap().as_ref() {
-            let mut new_search_nodes = vec![];
+            let mut expansions = vec![];
             for method in t.methods.iter() {
                 let new_tn = self.tn.decompose(task_id, method);
                 let new_search_node = SearchNode::new(
                     Rc::clone(&self.state),
                     Rc::new(new_tn),
                 );
-                new_search_nodes.push(new_search_node);
+                let expansion = NodeExpansion {
+                    connection_label: ConnectionLabel::Decomposition,
+                    items: vec![new_search_node],
+                    connector: Connector::OR
+                };
+                expansions.push(expansion);
             }
-            return new_search_nodes
+            return expansions
         }   
         unreachable!()     
     } 
@@ -122,4 +124,100 @@ pub struct NodeExpansion {
 pub enum ConnectionLabel {
     Execution(u32), 
     Decomposition
+}
+
+impl ConnectionLabel {
+    pub fn is_decomposition(&self) -> bool {
+        match  &self {
+            ConnectionLabel::Decomposition => true,
+            _ => false
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use crate::task_network::Method;
+    use super::*;
+
+    #[test]
+    pub fn expansion_correctness_test() {
+        let p1 = Rc::new(Task::Primitive(PrimitiveAction::new(
+            "p1".to_string(), 
+            1, 
+            HashSet::from([0]), 
+            vec![HashSet::from([1]), HashSet::from([2,4])],
+            vec![HashSet::from([3]), HashSet::new()] 
+        )));
+        let p2 = Rc::new(Task::Primitive(PrimitiveAction::new(
+            "p2".to_string(), 
+            1, 
+            HashSet::from([1, 2, 4]), 
+            vec![HashSet::from([1]),],
+            vec![HashSet::from([3]),] 
+        )));
+        let p3 = Rc::new(Task::Primitive(PrimitiveAction::new(
+            "p3".to_string(), 
+            1, 
+            HashSet::from([0, 3]), 
+            vec![HashSet::from([1]),],
+            vec![HashSet::from([3]),] 
+        )));
+        let p4 = Rc::new(Task::Primitive(PrimitiveAction::new(
+            "p4".to_string(), 
+            1, 
+            HashSet::from([4]), 
+            vec![HashSet::from([2]),],
+            vec![HashSet::new(),] 
+        )));
+        let m1 = Method::new(
+            "m1".to_string(),
+            HTN::new(
+                HashSet::from([1,2]), vec![(1,2)], HashMap::from([(1, p1.clone()), (2, p2.clone())]))
+        );
+        let m2 = Method::new(
+            "m2".to_string(),
+            HTN::new(
+                HashSet::from([1,2]), vec![], HashMap::from([(1, p3.clone()), (2, p4.clone())]))
+        );
+        let t1 = Rc::new(Task::Compound(CompoundTask::new(
+            "t1".to_string(),
+            vec![m1, m2]
+        )));
+
+        let tn = HTN::new(
+            HashSet::from([1, 2, 3, 4]), 
+            vec![(1,4), (2,4), (3,4)],
+            HashMap::from([
+                (1, p1), (2, t1), (3, p3), (4, p4)
+            ])
+        );
+        let state = HashSet::from([0,3]);
+        let sn = SearchNode::new(Rc::new(state), Rc::new(tn));
+        let expansion = sn.expand();
+        assert_eq!(expansion.len(), 4);
+        let exp_p1: Vec<&NodeExpansion> = expansion.iter().filter(|x| {
+            match x.connection_label {
+                ConnectionLabel::Execution(1) => true,
+                _ => false
+            }
+        }).collect();
+        assert_eq!(exp_p1.len(), 1);
+        match exp_p1[0].connector {
+            Connector::AND => {}
+            _ => panic!()
+        };
+        assert_eq!(exp_p1[0].items.len(), 2);
+        for node in exp_p1[0].items.iter() {
+            assert_eq!(node.tn.count_tasks(), 3);
+        }
+        let exp_t1: Vec<&NodeExpansion> = expansion.iter().filter(|x| {
+            match x.connection_label {
+                ConnectionLabel::Decomposition => true,
+                _ => false
+            }
+        }).collect();
+        assert_eq!(exp_t1.len(), 2);
+    }
 }
